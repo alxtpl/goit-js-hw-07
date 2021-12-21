7737e9598bbf0702429e4b6541288bb51737c18d
alxtpl <alxtpl@gmail.com>
2021-12-22 00:24:48 +0200

    adding markup and link

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const listGalleryNode = document.querySelector(".gallery");
+
+const galleryAdd = () => {
+    const galleryReturn = galleryItems
+        .map(({ preview, original, description }) => {
+            return `<li class="gallery__item">
+                         <a class="gallery__link" href="${original}">
+                            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/> </a>
+                    </li>`;
+        })
+        .join("");
+
+    listGalleryNode.insertAdjacentHTML("afterbegin", galleryReturn);
+};
+
+listGalleryNode.addEventListener("click", bigImageXtr);
+
+function bigImageXtr(e) {
+    const dataSource = e.target.dataset.source;
+    e.preventDefault();
+    if (e.target.tagName !== "IMG") return;
+    console.log(dataSource);
+}
+
+galleryAdd();
